392031a0e654512c35f35c1666649e940b60863f
DennisMufasa <denny.muasa@gmail.com>
2019-09-18 20:05:35 +0300

    chore: edit readme.md

--- readme.md
+++ readme.md
@@ -8,3 +8,15 @@ Basic concepts of mongodb a NoSQL database that includes installing the database
 5. Create a python virtual environment for me `python3 -m venv env` (If using IDE ignore step 5)
 6. Download the python driver for mongodb called PyMongo (for me `python -m pip install pymongo`)
 7. Create a python file and run this `import pymongo` code to test successful installation of pymongo if no errors occur.
+
+
+## Tasks to accomplish
+1. Create a database using mongodb
+2. call it students
+3. create collections for student personal info
+4. create collections for student academic progress
+5. create collections for student fee completion 
+6. Populate all the collections with documents
+7. fetch all student names and their courses
+8. fetch all students below 20 years of age
+9. fetch all students whose fee balance is more than half the agreed amount
--- students.py
+++ students.py
@@ -15,12 +15,13 @@ personal_info = Students["info"]
 academics = Students["academics"]
 school_fees = Students["fees"]
 
-# populate the collections with documents
-insert_personal_info = personal_info.insert_many(students_personal_info)
-insert_academic_details = academics.insert_many(students_academics_details)
-insert_fees_details = school_fees.insert_many(students_fees_details)
+# # populate the collections with documents
+# insert_personal_info = personal_info.insert_many(students_personal_info)
+# insert_academic_details = academics.insert_many(students_academics_details)
+# insert_fees_details = school_fees.insert_many(students_fees_details)
 
-print("personal details ids - ",insert_personal_info.inserted_ids)
-print("academics details ids - ",insert_academic_details.inserted_ids)
-print("fees details id - ",insert_fees_details.inserted_ids)
+# print("personal details ids - ",insert_personal_info.inserted_ids)
+# print("academics details ids - ",insert_academic_details.inserted_ids)
+# print("fees details id - ",insert_fees_details.inserted_ids)
 
+# after inserting the documents comment out the code to avoid duplicate entires
